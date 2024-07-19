b23824a03c76254f0329bb1e6e9d984220eee1bf
Shubhankar Kahali <shubhankar.kahali20@ibsindia.org>
2024-07-19 10:33:51 +0530

    gf

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { LinkedinScraper, events, typeFilter } = require('linkedin-jobs-scraper');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000; // Use the PORT environment variable
 
 app.use(express.json());
 
